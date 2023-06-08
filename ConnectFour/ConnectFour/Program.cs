12d0634054d6ed2bea348f2710f665c761de52ec
Pmacdon15 <p.macdonald038@mybvc.ca>
2023-06-08 12:24:29 -0600

    Initial commit

---
+++ ConnectFour/ConnectFour/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConnectFour
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
